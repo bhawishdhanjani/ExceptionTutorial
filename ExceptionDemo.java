946eb90c94e8d8cdacfbc2a89fea4677d78332b1
Bhavesh Kumar <bhawishdhanjanii@gmail.com>
2023-02-08 18:43:38 +0500

    Catching the Exception

--- ExceptionDemo.java
+++ ExceptionDemo.java
@@ -1,12 +1,20 @@
 package ExceptionTutorial;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class ExceptionDemo {
     public static void show(){
-        sayHello(null);
+        try {
+            var fileReader = new FileReader("file1.txt");
+            System.out.println("File is opened");
+        }
+        catch (FileNotFoundException ex){
+            System.out.println(ex.getMessage());
+        }
 
     }
     public static void sayHello(String name){
         System.out.println(name.toUpperCase());
-
     }
 }
